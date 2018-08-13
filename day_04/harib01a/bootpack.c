04f5e5df21db025b323edb7d5ce9ec4a165ac9c5
Keita Nakagami <keita00@gmail.com>
2018-08-13 12:55:25 +0900

    day4 harib01a
    かなり全体に修正
    c言語からアセンブラを読みだすときはアンダーラインはC言語から見たら削除されていないので、全く同じ名前で定義する
    Makefile全体に修正

---
+++ day_04/harib01a/bootpack.c
@@ -0,0 +1,17 @@
+extern void _io_hlt(void);  /*他のファイルにこの名前の関数があるよと宣言*/
+extern void _write_mem8(int addr, int data);
+
+void HariMain(void)
+{
+    int i;
+
+    for ( i = 0xa000; i <= 0xaffff; i++ ) {
+        _write_mem8( i, 15);
+    }
+
+
+    for( ; ; ) {
+        _io_hlt();
+    }
+    
+}
