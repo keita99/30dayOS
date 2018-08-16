6f0600928f6b037ad5cab3a4e664cdaca2a94811
Keita Nakagami <keita00@gmail.com>
2018-08-17 00:45:03 +0900

    day04 harib01g add

--- day_04/harib01g/bootpack.c
+++ day_04/harib01g/bootpack.c
@@ -7,20 +7,37 @@ extern void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+#define COL8_000000     0
+#define COL8_FF0000     1
+#define COL8_00FF00     2
+#define COL8_FFFF00     3
+#define COL8_0000FF     4
+#define COL8_FF00FF     5
+#define COL8_00FFFF     6
+#define COL8_FFFFFF     7
+#define COL8_C6C6C6     8
+#define COL8_840000     9
+#define COL8_008400     10
+#define COL8_848400     11
+#define COL8_000084     12
+#define COL8_840084     13
+#define COL8_008484     14
+#define COL8_848484     15
+
+
 void HariMain(void)
 {
-
-    int i;
     char *p;
 
     init_palette();
 
     p = (char *) 0xa0000;
 
-    for ( i = 0; i <= 0xffff; i++ ) {
-        p[i] = i & 0x0f;
-    }
-
+    boxfill8(p, 320, COL8_FF0000, 100, 50, 150,100);
+    boxfill8(p, 320, COL8_FF00FF, 60, 20, 150,120);
+    boxfill8(p, 320, COL8_848400, 50, 20, 100,700);
 
     for (;;) {
         io_hlt();
@@ -64,5 +81,16 @@ void set_palette(int start, int end, unsigned char *rgb)
         rgb += 3;
     }
     io_store_eflags(eflags);    /* 割り込み許可フラグを復元 */
+    return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
+{
+    int x, y;
+    for ( y = y0; y <= y1; y++) {
+        for ( x = x0; x <= x1; x++) {
+            vram[y * xsize + x] = c;
+        }
+    }
     return;
 }
