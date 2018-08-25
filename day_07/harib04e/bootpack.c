024e84f1b5e5604a7e91d550949b76dcbdbcfc4c
Keita Nakagami <keita00@gmail.com>
2018-08-25 15:17:26 +0900

    整理

--- day_07/harib04e/bootpack.c
+++ day_07/harib04e/bootpack.c
@@ -1,10 +1,12 @@
 #include "bootpack.h"
 
+extern struct KEYBUF keybuf;
+
 void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
     char s[40], mcursor[256];
-    int mx, my;
+    int mx, my, i, j;
 
     init_gdtidt();
     init_pic();
@@ -26,6 +28,19 @@ void HariMain(void)
     io_out8(PIC1_IMR, 0xef); /* マウスを許可 */
 
     for (;;) {
-        io_hlt();
+        io_cli();
+        if (keybuf.next == 0) {
+            io_stihlt();
+        } else {
+            i = keybuf.data[0];
+            keybuf.next--;
+            for (j = 0; j <keybuf.next; j++) {
+                keybuf.data[i] =keybuf.data[j + 1];
+            }
+            io_sti();
+            sprintf(s, "%x", i);
+    	    boxfill8(binfo->vram, binfo->scrnx, COL8_008484 , 0, 16, 15, 31);
+	        putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+        }
     }  
 }
