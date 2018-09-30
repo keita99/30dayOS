6754f6c8a968a0afd366c26afdef3d5ecb1be32e
Keita Nakagami <keita00@gmail.com>
2018-09-30 16:40:03 +0900

    harib10i 番兵を使ってのtimerの改良

--- day_13/harib10i/timer.c
+++ day_13/harib10i/timer.c
@@ -10,17 +10,22 @@ struct TIMERCTL timerctl;
 void init_pit(void)
 {
     int i;
+    struct TIMER *t;
 
     io_out8(PIT_CTRL,0x34);
     io_out8(PIT_CNT0,0x9c); /* 割り込み周期 11932回（１秒間に１００回割り込み)(0x2e9c) */
     io_out8(PIT_CNT0,0x2e);
 
     timerctl.count = 0;
-    timerctl.next = 0xffffffff;
-    timerctl.using = 0;
     for (i = 0; i < MAX_TIMER; i++) {
         timerctl.timers0[i].flags = 0;
     }
+    t = timer_alloc();
+    t->timeout = 0xffffffff;
+    t->flags = TIMER_FLAGS_USING;
+    t->next = 0;/* 最後 */
+    timerctl.t0 = t;/* でも先頭 */
+    timerctl.next = 0xffffffff;
     return;    
 }
 
@@ -50,7 +55,6 @@ void timer_init(struct TIMER *timer, struct FIFO32 *fifo, int data)
 
 void inthandler20(int *esp)
 {
-    int i;
     struct TIMER *timer;
     io_out8(PIC0_OCW2, 0x60);
 
@@ -62,7 +66,7 @@ void inthandler20(int *esp)
 
     timer = timerctl.t0;    
 
-    for (i = 0; i < timerctl.using; i++) {
+    for (;;) {
         if (timer->timeout > timerctl.count) {
             break;
         }
@@ -72,15 +76,9 @@ void inthandler20(int *esp)
         timer = timer->next;
     }
 
-    timerctl.using -= i;
-
     timerctl.t0 = timer;
 
-    if (timerctl.using > 0) {
-        timerctl.next = timerctl.t0->timeout;
-    } else {
-        timerctl.next = 0xffffffff;
-    }
+    timerctl.next = timerctl.t0->timeout;
 
     return;
 }
@@ -93,17 +91,6 @@ void timer_settime(struct TIMER *timer, unsigned int timeout)
     timer->flags = TIMER_FLAGS_USING;
     e = io_load_eflags();
     io_cli();
-
-    timerctl.using++;
-
-    if (timerctl.using == 1) {
-        /* タイマが一個の場合 */
-        timerctl.t0 = timer;
-        timer->next = 0;
-        timerctl.next = timer->timeout;
-        io_store_eflags(e);
-        return;
-    }
 
     t = timerctl.t0;
     if (timer->timeout <= t->timeout) {
@@ -131,9 +118,4 @@ void timer_settime(struct TIMER *timer, unsigned int timeout)
         }
     }
 
-    /* 一番後ろに入れる */
-    s->next = timer;
-    timer->next = 0;
-    io_store_eflags(e);
-    return;
 }
